181e342c29cc5fca92b517ec63df8cb31bf64c41
Rohan Varma <rohan@Rohans-MBP.wv.cc.cmu.edu>
2017-04-06 14:24:42 -0400

    pcl setup

---
+++ cplusplus/build/CMakeFiles/pcd_write_test.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rohan/Dropbox/Work/workspace/pointcloud/pcseg/cplusplus/pcd_write.cpp" "/Users/rohan/Dropbox/Work/workspace/pointcloud/pcseg/cplusplus/build/CMakeFiles/pcd_write_test.dir/pcd_write.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingFreeType_AUTOINIT=1(vtkRenderingFreeTypeFontConfig)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/vtk/7.1.1/include/vtk-7.1"
+  "/usr/local/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7"
+  "/usr/local/Cellar/hdf5/1.10.0-patch1/include"
+  "/usr/local/opt/szip/include"
+  "/usr/include/libxml2"
+  "/usr/local/include/pcl-1.8"
+  "/usr/local/Cellar/eigen/3.3.3/include/eigen3"
+  "/System/Library/Frameworks/Python.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
