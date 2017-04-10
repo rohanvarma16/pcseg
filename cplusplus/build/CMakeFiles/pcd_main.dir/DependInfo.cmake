3ff05e82b1c4e09ca6005d3601b8128f0e48896c
rohanvarma16 <rohanvarma16@gmail.com>
2017-04-10 11:22:53 -0400

    cleanup

---
+++ cplusplus/build/CMakeFiles/pcd_main.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rohan/Dropbox/Work/workspace/pointcloud/pcseg/cplusplus/pcd_main.cpp" "/Users/rohan/Dropbox/Work/workspace/pointcloud/pcseg/cplusplus/build/CMakeFiles/pcd_main.dir/pcd_main.cpp.o"
+  "/Users/rohan/Dropbox/Work/workspace/pointcloud/pcseg/cplusplus/viz.cpp" "/Users/rohan/Dropbox/Work/workspace/pointcloud/pcseg/cplusplus/build/CMakeFiles/pcd_main.dir/viz.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingFreeType_AUTOINIT=1(vtkRenderingFreeTypeFontConfig)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/vtk/7.1.1/include/vtk-7.1"
+  "/usr/local/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7"
+  "/usr/local/Cellar/hdf5/1.10.0-patch1/include"
+  "/usr/local/opt/szip/include"
+  "/usr/include/libxml2"
+  "/usr/local/include/pcl-1.8"
+  "/usr/local/Cellar/eigen/3.3.3/include/eigen3"
+  "/usr/local/Cellar/flann/1.9.1_3/include"
+  "/System/Library/Frameworks/Python.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cplusplus/build/CMakeFiles/pcd_main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd_main.dir/viz.cpp.o"
+  "CMakeFiles/pcd_main.dir/pcd_main.cpp.o"
+  "pcd_main.pdb"
+  "pcd_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
